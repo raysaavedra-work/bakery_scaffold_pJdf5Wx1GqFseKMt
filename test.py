5b6e1adb57fc119b93b1cf318f3648bd9806a988
raysaavedra-work <raysaavedra.work@gmail.com>
2019-10-02 08:26:46 +0000

    Updated tests for assessment

--- test.py
+++ test.py
@@ -201,7 +201,7 @@ class AssessmentTestCases(unittest.TestCase):
         return res.group().split("'")[1]
 
     def _get_url(self):
-        return "https://calm-ravine-44798.herokuapp.com/"
+        return "https://secret-mesa-77766.herokuapp.com/"
 
     def _check_webhook_data(self):
         x = urllib.request.urlopen(f'{self._get_url()}/payment_intent')
